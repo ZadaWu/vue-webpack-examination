7e60b8de51749d97ea22a12a61543f0636e79eff
zhangming.wu <zada.wu@gmail.com>
2019-11-26 16:22:34 +0800

    feat:增加css解析器

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import avator from './open-btn.png';
+import './index.css';
 
 let img = new Image();
 img.src = avator;
+img.classList.add('avatar')
 
 let root = document.getElementById('root');
 root.append(img);
--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
         rules: [{
             test: /\.(png|svg|jpg|gif)$/,
             use: [
-                // url-loader对于图片较小的可以用base64进行压缩，剩下的大图片进入file-loader进行处理
+                // url-loader对于图片较小的可以用base64进行压缩，剩下的大图片进入直接放到指定文件夹
                 {
                     loader: "url-loader",
                     options: {
@@ -19,15 +19,13 @@ module.exports = {
                         outputPath: 'images/',
                         limit: 2048
                     }
-                },
-                {
-                    loader: 'file-loader',
-                    // 放到指定的文件夹，并且对名字进行处理
-                    options: {
-                        name: '[name].[ext]',
-                        outputPath: 'images/'
-                    }
-            }]
+                }
+            ]
+        }, {
+            // css-loader会帮我们分析出所有css文件之间的关系， 最终把这些css文件合并成一段css，
+            // style-loader在得到css-loader生成的内容之后，style-loader会把这段内容挂载到页面的head部分
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader']
         }]
     }
 }
