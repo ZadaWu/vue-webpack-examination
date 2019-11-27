6c5c2806ee4d59aa4ea68139add85ec11cba1926
zhangming.wu <zada.wu@gmail.com>
2019-11-27 11:29:14 +0800

    feat:热更新

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 var root = document.getElementById('root');
 import './index.scss'
 root.innerHTML = '<i class="web-font">明月几时有，自己抬头瞅</i>'
+console.log('hhhh')
--- webpack.config.js
+++ webpack.config.js
@@ -4,9 +4,32 @@ const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
 module.exports = {
     mode: "production",
+    // 在development模式,用cheap-module-eval-source-map; 在production模式下，用cheap-module-source-map
+    devtool: 'cheap-module-eval-source-map',
+    // 在 localhost:8080 下建立服务，将 dist 目录下的文件，作为可访问文件。
+    devServer: {
+        contentBase: './dist',
+        open: true, //自动打开页面
+        port: 8080, // 设置端口号
+        // 如果你有单独的后端开发服务器 API，并且希望在同域名下发送 API 请求 ，那么代理某些 URL 会很有用
+        // 在 localhost:3000 上有后端服务的话，你可以这样启用代理：
+        // 请求到 /api/users 现在会被代理到请求 http://localhost:3000/api/users
+        proxy: {
+            '/api': 'http://localhost:3000'
+        }
+
+    },
+    performance: {
+        hints: process.env.NODE_ENV === 'production' ? "warning" : false
+    },
     entry: {
-        main: './src/index.js',
-        sub: './src/index.js'
+        main: './src/index.js'
+        // sub: './src/index.js'
+    },
+    optimization: {
+        splitChunks: {
+            chunks: 'all'
+        }
     },
     output: {
         filename: "[name].js", //name 这里name指的就是前面entry中对应的main和sub
