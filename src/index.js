d9a47cfc4b6bb01ab4ca0bd241db0eff6a28d5b8
zhangming.wu <zada.wu@gmail.com>
2019-12-20 19:55:49 +0800

    feat:使用webpackPrefetch来使主要js加载完了以后，再去偷偷加载好剩余的文件

--- src/index.js
+++ src/index.js
@@ -23,16 +23,22 @@
 // ele.innerHTML = _.join(['Hello', 'World'], '-');
 // document.body.appendChild(ele);
 
-function getComponent() {
-    return import(/*webpackChunkName:"lodash" */'lodash').then(_ => {
-        var ele = document.createElement('div');
-        ele.innerHTML = _.join(['Hello', 'World'], '-');
-        return ele;
-    })
-}
-
+// function getComponent() {
+//     return import(/*webpackChunkName:"lodash" */'lodash').then(_ => {
+//         var ele = document.createElement('div');
+//         ele.innerHTML = _.join(['Hello', 'World'], '-');
+//         return ele;
+//     })
+// }
+//
+// document.addEventListener('click', () => {
+//     getComponent().then(ele => {
+//         document.body.appendChild(ele);
+//     })
+// })
+// webpackPrefetch: true 会等你主要的 JS 都加载完了之后，网络带宽空闲的时候，它就会预先帮你加载好
 document.addEventListener('click', () => {
-    getComponent().then(ele => {
-        document.body.appendChild(ele);
+    import(/* webpackPrefetch: true */'./click.js').then(({default: func}) => {
+        func();
     })
 })
